d15fecf813ad6fefa314757e5add31d3058edd18
Ronit Rudra <ronit.rudra1995@gmail.com>
2017-07-22 14:13:40 -0500

    ScrollView Basics
    Learning how to scroll content using ScrollView and doing some frame math.

--- PageScroll/ViewController.swift
+++ PageScroll/ViewController.swift
@@ -9,11 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    // Declare an empty array of type UIImageView
+    var images = [UIImageView]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        
+        // Loop through the 3 icon images
+        for x in 0...2{
+            // Inititalize a constant with image of icon
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            // append read image to array
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            // Offset images to outside of screen, right at the center
+            // x is typecasted as newX is a CGFloat
+            // new math based on changed scrollview, not the superview
+            newX = scrollView.frame.size.width/2 + scrollView.frame.size.width * CGFloat(x)
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX-75,y: (scrollView.frame.size.height/2)-75,width: 150, height: 150)
+        }
+        
+        // Display size of image array in console
+        print("Count: \(images.count)")
+        
+        //scrollView.backgroundColor = UIColor.purple
+        scrollView.clipsToBounds = false
+        
+        scrollView.contentSize = CGSize(width: contentWidth,height: view.frame.size.height)
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
